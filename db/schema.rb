5e8f658f5c22959e8e30750e920ff9c1605f1c9e
xxyychyo <103950402+xxyychyo@users.noreply.github.com>
2022-05-20 15:09:35 +0900

    修正２

--- db/schema.rb
+++ db/schema.rb
@@ -53,14 +53,6 @@ ActiveRecord::Schema.define(version: 2022_05_18_110420) do
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
-  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
